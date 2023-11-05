a6c0b15ac3a621c92039725eb876f1c552b46d80
BenLevintan <benlevintan@gmail.com>
2023-11-05 11:58:10 +0200

    Update LinkedList.py
    added the insertHead and insertTail functions to the LinkedList class

--- python projects/dashboard app/LinkedList.py
+++ python projects/dashboard app/LinkedList.py
@@ -13,10 +13,14 @@ class LinkedList:
 
 
     def insertHead(self, val: int) -> None:
-        
+        new_node = ListNode(val)      # creating a new node with given val
+        new_node.next = self.head     # pushing the head nude as the next node to the new nude
+        self.head = new_node          # setting the new node as head of the list
 
     def insertTail(self, val: int) -> None:
-        
+        new_node = ListNode(val)      # creating a new node with given val
+        self.tail.next = new_node     # putting the new node at the end of the list (after the tail)
+        self.tail = new_node          # making the new node at the tail of the list
 
     def remove(self, index: int) -> bool:
         
