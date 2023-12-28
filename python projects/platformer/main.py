e48cdc307187bc8583722e0c1c380fc9fb2daa0e
BenLevintan <benlevintan@gmail.com>
2023-12-28 20:44:05 +0200

    main.py update
    -fixed asset loading

--- python projects/platformer/main.py
+++ python projects/platformer/main.py
@@ -15,9 +15,38 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
-class Player(pygame.sprite.Sprite):  #inherating from sprite 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
 
+#loading all sprite in the game(incloding facing left and right)
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
+class Player(pygame.sprite.Sprite):  #inherating from sprite 
     COLOR = (255, 0 ,0)
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -26,7 +55,8 @@ class Player(pygame.sprite.Sprite):  #inherating from sprite
         self.mask = None 
         self.direction = "left"
         self.animation_count = 0
-    
+        self.fall_count = 0
+
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
@@ -34,20 +64,24 @@ class Player(pygame.sprite.Sprite):  #inherating from sprite
     def move_left(self, vel):
         self.x_vel = -vel
         if self.direction != "left":
-            self.diraction = "left"
+            self.direction = "left"
             self.animation_count = 0
 
     def move_right(self, vel):
         self.x_vel =vel 
         if self.direction != "right":
-            self.diraction = "right"
+            self.direction = "right"
             self.animation_count = 0
 
     def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
+        #self.fall_count += 1
+
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 def get_background(name):
@@ -75,7 +109,7 @@ def draw(window, background, bg_image, player):
 def handle_move(player):
     keys = pygame.key.get_pressed()
 
-    #player.x_vel = 0
+    player.x_vel = 0
     if keys[pygame.K_LEFT]:
         player.move_left(PLAYER_VEL)
     if keys[pygame.K_RIGHT]:
@@ -100,7 +134,6 @@ def main(window):
         player.loop(FPS)
         handle_move(player)
         draw(window, background, bg_image, player)
-
 
     pygame.quit()
     quit()
