5ef9fa7fbb638a23bad04062914ffc6a88f250b6
BenLevintan <benlevintan@gmail.com>
2024-01-02 21:27:40 +0200

    bug fixes
    - removed the creation of a Player object from the game loop
    - added terminal velocity to player free fall
    - added load_sprite_sheets() function, won't compile due to an error

--- python projects/platformer/main.py
+++ python projects/platformer/main.py
@@ -1,11 +1,11 @@
-# main
-
+# main.py
 import os
 import random
 import math
 import pygame
 from utils import get_background, draw
 from player import Player
+
 pygame.init()
 
 pygame.display.set_caption("Platformer")
@@ -15,28 +15,26 @@ FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-player = Player(100, 100, 50, 50)
 
 def main(window):
     clock = pygame.time.Clock()
 
+    player = Player(100, 100, 50, 50)
+
     run = True
     while run:
         clock.tick(FPS)
-
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
-        backgound = get_background(window, "level1.png")
-
-        player.loop(FPS)
         player.handle_move()
-        draw(window, backgound, player)
-        print(player.rect)
+        player.loop(FPS)
+
+        background = get_background(window, "level1.png")
+        draw(window, background, player)
         # Update the display
         pygame.display.update()
 
--- python projects/platformer/player.py
+++ python projects/platformer/player.py
@@ -1,9 +1,10 @@
 # player
 import pygame
+from utils import load_sprite_sheets
 
 pygame.init()
 
-PLAYER_VEL = 5
+
 
 def print_function_name(func):
     def wrapper(*args, **kwargs):
@@ -12,6 +13,14 @@ def print_function_name(func):
     return wrapper
 
 class Player(pygame.sprite.Sprite):
+
+    PLAYER_VEL = 5
+    GRAVITY = 1
+    TERMINAL_VELOCITY = 10
+
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
+    ANIMATION_DELAY = 3
+
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
@@ -20,6 +29,7 @@ class Player(pygame.sprite.Sprite):
         self.direction = "right"
         self.animation_count = 0
         self.COLOR = 233, 23, 23
+        self.fall_count = 0 #counts the frames of which player been falling
 
 
     def move(self, dx, dy):
@@ -42,7 +52,12 @@ class Player(pygame.sprite.Sprite):
 
 
     def loop(self, fps):
+        self.y_vel += min (1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
+        self.y_vel = min(self.y_vel, self.TERMINAL_VELOCITY)
+
+        self.fall_count += 2
+
 
     def draw(self, window):
         pygame.draw.rect(window, self.COLOR, self.rect)
@@ -53,6 +68,6 @@ class Player(pygame.sprite.Sprite):
 
         self.x_vel = 0
         if keys[pygame.K_LEFT]:
-            self.move_left(PLAYER_VEL)
+            self.move_left(self.PLAYER_VEL)
         if keys[pygame.K_RIGHT]:
-            self.move_right(PLAYER_VEL)
+            self.move_right(self.PLAYER_VEL)
